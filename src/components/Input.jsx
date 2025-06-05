130f18a0f0c50da9babfc20dfdfcba7d9ab1aa19
Bariharsh <144584767+Bariharsh@users.noreply.github.com>
2025-06-05 14:04:44 +0530

    all done just image problem

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,15 +4,21 @@ const Input = React.forwardRef(function Input(
   { label, type = "text", className = "", ...props },
   ref
 ) {
-    const Id = useId();
+  const id = useId();
   return (
     <div className="w-full">
-      {label && 
+      {label && (
         <label className="block mb-1 pl-1" htmlFor={id}>
           {label}
         </label>
-      }
-      <input type={type} className={` px-3 py-2 border rounded-lg w-full text-black bg-white outine-none focus:bg-gray-50 duration-200 ${className}`} ref={ref} {...props} id={id}/>
+      )}
+      <input
+        type={type}
+        className={` px-3 py-2 border rounded-lg w-full text-black bg-white outine-none focus:bg-gray-50 duration-200 ${className}`}
+        ref={ref}
+        {...props}
+        id={id}
+      />
     </div>
   );
 });
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,83 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Pages/Home'
+
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+import AllPosts from './Pages/AllPosts'
+import AddPost from './Pages/AddPost'
+import EditPost from './Pages/EditPost'
+import Post from './Pages/Post'
+import AuthLayout from './components/AuthLayout'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            path: "/",
+            element: <Home/>,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Login />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Signup />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPosts />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AddPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router}/>
     </Provider>
   </StrictMode>,
 )
